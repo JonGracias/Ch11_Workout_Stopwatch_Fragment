243aca5238f8456648abaf8ecda8d4ecd81993d6
Jon Gracias <jonpgracias@gmail.com>
2021-04-10 23:08:30 -0400

    final commit

--- app/src/main/java/com/hfad/workout/WorkoutListFragment.java
+++ app/src/main/java/com/hfad/workout/WorkoutListFragment.java
@@ -1,5 +1,6 @@
 package com.hfad.workout;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,8 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class WorkoutListFragment extends ListFragment {
+
+    static interface Listener {
+        void itemClicked(long id);
+    };
+    private Listener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -26,5 +33,18 @@ public class WorkoutListFragment extends ListFragment {
         setListAdapter(adapter);
 
         return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (Listener)context;
+    }
+
+    @Override
+    public void onListItemClick(ListView listView, View itemView, int position, long id) {
+        if (listener != null) {
+            listener.itemClicked(id);
+        }
     }
 }
